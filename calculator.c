73f4bef7d66cb18cf81dd3f3654e6b53d9b70339
AakashEmandi <65613417+Aakashpspk17@users.noreply.github.com>
2021-08-08 19:00:17 +0530

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -4,4 +4,17 @@ int main()
 {
     float num1, num2, res;
     int choice;
+     printf("1. Addition\n");
+        printf("2. Subtraction\n");
+        printf("3. Multiplication\n");
+        printf("4. Division\n");
+        printf("5. Exit\n\n");
+        printf("Enter Your Choice(1-5): ");
+        scanf("%d", &choice);
+        if(choice>=1 && choice<=4)
+        {
+            printf("\nEnter any two Numbers: ");
+            scanf("%f %f", &num1, &num2);
+        }
+        switch(choice)
  }
